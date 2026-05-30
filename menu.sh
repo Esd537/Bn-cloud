e57e0508a35c0eea6f76d7bdb9e56b33b245b4a7
Bn <116313128+Esd537@users.noreply.github.com>
2026-05-30 14:46:11 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -21,7 +21,7 @@ AIRLINK_URL="https://raw.githubusercontent.com/Esd537/Bn-cloud/refs/heads/main/a
 PTERODACTYL_UPDATER_URL="https://raw.githubusercontent.com/Esd537/Bn-cloud/refs/heads/main/updater.sh"
 WINGS_URL="https://raw.githubusercontent.com/Esd537/Bn-cloud/refs/heads/main/wings.sh"
 BLUEPRINT_URL="https://raw.githubusercontent.com/Esd537/Bn-cloud/refs/heads/main/blueprint.sh"
-DEPS_URL="https://raw.githubusercontent.com/Esd537/Bn-cloud/refs/heads/main/deps.sh"
+SETUP_URL="https://raw.githubusercontent.com/Esd537/Bn-cloud/refs/heads/main/setup.sh"
 
 # --- Garante que o teclado funcione ---
 exec < /dev/tty
